5c4905016a8ea32bb7f5f3e0862b48bf48ccb16a
XiaShu <2453893123@qq.com>
2020-02-01 22:49:49 +0800

    🎈 前后端 后端提供服务

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/CatalogServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/CatalogServiceImpl.java
@@ -2,11 +2,15 @@ package com.atguigu.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.gmall.bean.PmsBaseCatalog1;
+import com.atguigu.gmall.bean.PmsBaseCatalog2;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
+import com.atguigu.gmall.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.manage.mapper.PmsBaseCatalog1Mapper;
 import com.atguigu.gmall.manage.mapper.PmsBaseCatalog2Mapper;
 import com.atguigu.gmall.manage.mapper.PmsBaseCatalog3Mapper;
 import com.atguigu.gmall.service.CatalogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
 
@@ -26,5 +30,22 @@ public class CatalogServiceImpl implements CatalogService {
     public List<PmsBaseCatalog1> getCatalog1() {
 
         return pmsBaseCatalog1Mapper.selectAll();
+    }
+
+    @Override
+    public List<PmsBaseCatalog2> getCatalog2(String catalog1Id) {
+        // 条件筛选
+        PmsBaseCatalog2 pmsBaseCatalog2 = new PmsBaseCatalog2();
+        pmsBaseCatalog2.setCatalog1Id(catalog1Id);
+        List<PmsBaseCatalog2> catalog2s = pmsBaseCatalog2Mapper.select(pmsBaseCatalog2);
+        return catalog2s;
+    }
+
+    @Override
+    public List<PmsBaseCatalog3> getCatalog3(String catalog2Id) {
+        PmsBaseCatalog3 pmsBaseCatalog3 = new PmsBaseCatalog3();
+        pmsBaseCatalog3.setCatalog2Id(catalog2Id);
+        List<PmsBaseCatalog3> catalog3s = pmsBaseCatalog3Mapper.select(pmsBaseCatalog3);
+        return catalog3s;
     }
 }
--- gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/CatalogController.java
@@ -2,14 +2,20 @@ package com.atguigu.gmall.manage.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.atguigu.gmall.bean.PmsBaseCatalog1;
+import com.atguigu.gmall.bean.PmsBaseCatalog2;
+import com.atguigu.gmall.bean.PmsBaseCatalog3;
 import com.atguigu.gmall.service.CatalogService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
 @Controller
+// 解决跨域问题
+@CrossOrigin
 public class CatalogController {
 
     @Reference
@@ -23,5 +29,24 @@ public class CatalogController {
         return catalog1s;
 
     }
+
+    @ResponseBody
+    @RequestMapping("/getCatalog2")
+    public List<PmsBaseCatalog2> getCatalog2(String catalog1Id){
+        List<PmsBaseCatalog2> catalog2s = catalogService.getCatalog2(catalog1Id);
+        return catalog2s;
+    }
+
+    @ResponseBody
+    @RequestMapping("/getCatalog3")
+    public List<PmsBaseCatalog3> getCatalog3(String catalog2Id){
+        List<PmsBaseCatalog3> catalog3s = catalogService.getCatalog3(catalog2Id);
+        return catalog3s;
+    }
+
+
+
+
+
 
 }
